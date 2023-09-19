6372f300ef743a1b17fe471cd7eb8436563f346b
Bruno Cesar <bcesar.bruno@gmail.com>
2023-09-19 20:50:29 -0300

    feat: Primeiro commit

---
+++ src/test/java/dev/brunocesar/imovelsimplificado/advert/AdvertApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brunocesar.imovelsimplificado.advert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
